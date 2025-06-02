713c2b8dd431913897ab1e59a5069e157eaa56d8
MostafaMohamed2001 <mostafamohamed8828@gmail.com>
2025-06-02 13:19:17 +0300

    Add axios and node-cron dependencies; implement cron job to call test endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,22 @@
 const authUserRoutes = require("./authUserRoutes.js");
+const cron = require("node-cron");
+const axios = require("axios");
+
 module.exports = (app) => {
   app.use("/api/v1/auth", authUserRoutes);
+
+  // to free server not make a sleep
+  app.get("/test", (req, res) => {
+    res.send("Hello from world");
+  });
+  cron.schedule("* * * * *", () => {
+    axios
+      .get("http://localhost:3000/test")
+      .then((response) => {
+        console.log(`Called /test: ${response.status} - ${response.data}`);
+      })
+      .catch((error) => {
+        console.error("Error calling /test:", error.message);
+      });
+  });
 };
