f02dc1841d4b3ff1db354dc3af7122581b2378e4
MostafaMohamed2001 <mostafamohamed8828@gmail.com>
2025-06-02 15:16:40 +0300

    Remove axios and node-cron dependencies; eliminate cron job implementation from routes.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,4 @@
 const authUserRoutes = require("./authUserRoutes.js");
-const cron = require("node-cron");
-const axios = require("axios");
 
 module.exports = (app) => {
   app.use("/api/v1/auth", authUserRoutes);
@@ -8,15 +6,5 @@ module.exports = (app) => {
   // to free server not make a sleep
   app.get("/test", (req, res) => {
     res.send("Hello from world");
-  });
-  cron.schedule("* * * * *", () => {
-    axios
-      .get("http://localhost:3000/test")
-      .then((response) => {
-        console.log(`Called /test: ${response.status} - ${response.data}`);
-      })
-      .catch((error) => {
-        console.error("Error calling /test:", error.message);
-      });
   });
 };
